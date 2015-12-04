2c65d17bbf2ef1e40e690cb5b13f0c047ec91d74
Chris Vernon <christopher.vernon@sky.uk>
2015-12-04 13:54:19 +0000

    Basic Nav

--- js/myface.js
+++ js/myface.js
@@ -1,38 +1,138 @@
 (function(){
 
-    var grid = document.getElementById('site-grid');
-    var contents = '';
+    var grid = document.getElementById('site-grid'),
+        background = document.getElementById('site-background'),
+        siteLinks = document.getElementsByClassName('site-nav__link');
 
-    // create the people elements
-    skyFaceData.people.forEach(function(person, i){
+    function loadPeople(){
+        var contents = '';
+        background.className = '';
 
-        var str = '<div class="grid__item"';
+        // create the people elements
+        grid.innerHTML = '';
+        skyFaceData.people.forEach(function(person, i){
 
-        str += ' data-name="' + person.name + '"';
-        str += ' data-discipline="' + person.discipline + '"';
-        str += ' data-squad="' + person.squad.name + '"';
-        str += ' data-tribe="' + person.squad.tribe.name + '">';
+            var str = '<div class="grid__item"';
 
-        str += '<a class="tile">';
-        str += '<div class="tile__inner">';
+            str += ' data-name="' + person.name + '"';
+            str += ' data-discipline="' + person.discipline + '"';
+            str += ' data-squad="' + person.squad.name + '"';
+            str += ' data-tribe="' + person.squad.tribe.name + '">';
 
-        str += '<div class="tile__image-wrap">';
-        str += '<img class="tile__image" src="' + person.image +'">';
-        //
-        str += '</div>';
+            str += '<a class="tile">';
+            str += '<div class="tile__inner">';
 
-        str += '<div class="tile__body">';
-        str += '<h2 class="tile__heading">' + person.name + '</h2>';
-        str += '<h3 class="tile__sub-heading">Associate Software Developer</h3>';
-        str += '</div>';
+            str += '<div class="tile__image-wrap">';
+            str += '<img class="tile__image" src="' + person.image +'">';
+            //
+            str += '</div>';
 
-        str += '</div>';
-        str += '</a>';
-        str += '</div>';
+            str += '<div class="tile__body">';
+            str += '<h2 class="tile__heading">' + person.name + '</h2>';
+            str += '<h3 class="tile__sub-heading">Associate Software Developer</h3>';
+            str += '</div>';
 
-        contents += str;
-    });
+            str += '</div>';
+            str += '</a>';
+            str += '</div>';
 
-    grid.innerHTML = contents;
+            contents += str;
+        });
+
+        grid.innerHTML = contents;
+    }
+
+    function loadTribes(){
+        var contents = '';
+        background.className = 'active';
+
+        console.log("Load the tribes =)");
+
+        // create the tribes elements
+        grid.innerHTML = '';
+        skyFaceData.tribes.forEach(function(tribe, i){
+
+            var str = '<div class="grid__item"';
+
+            str += ' data-name="' + tribe.name + '">';
+
+            str += '<a class="tile">';
+            str += '<div class="tile__inner">';
+
+            str += '<div class="tile__image-wrap">';
+            str += '<img class="tile__image" src="' + tribe.image +'">';
+            //
+            str += '</div>';
+
+            str += '<div class="tile__body">';
+            str += '<h2 class="tile__heading">' + tribe.name + '</h2>';
+            str += '<h3 class="tile__sub-heading">Associate Software Developer</h3>';
+            str += '</div>';
+
+            str += '</div>';
+            str += '</a>';
+            str += '</div>';
+
+            contents += str;
+        });
+
+        grid.innerHTML = contents;
+
+    }
+
+    function loadSquads(){
+        var contents = '';
+        background.className = 'active';
+        console.log("Load the squads =)");
+
+        // create the tribes elements
+        grid.innerHTML = '';
+        skyFaceData.squads.forEach(function(squad, i){
+
+            var str = '<div class="grid__item"';
+
+            str += ' data-name="' + squad.name + '">';
+
+            str += '<a class="tile">';
+            str += '<div class="tile__inner">';
+
+            str += '<div class="tile__image-wrap">';
+            str += '<img class="tile__image" src="' + squad.image +'">';
+            //
+            str += '</div>';
+
+            str += '<div class="tile__body">';
+            str += '<h2 class="tile__heading">' + squad.name + '</h2>';
+            str += '<h3 class="tile__sub-heading">Associate Software Developer</h3>';
+            str += '</div>';
+
+            str += '</div>';
+            str += '</a>';
+            str += '</div>';
+
+            contents += str;
+        });
+
+        grid.innerHTML = contents;
+    }
+
+    function loadData(e){
+        var loadTarget = e.target.getAttribute('data-load');
+
+        if(loadTarget === "tribes"){
+            loadTribes();
+        }else if(loadTarget === "squads"){
+            loadSquads();
+        }else{
+            loadPeople();
+        }
+    }
+
+    // add the event to handle clicks
+    for(var i = 0; i < siteLinks.length; i++){
+        siteLinks[i].onclick = loadData;
+    }
+
+    loadPeople();
 
 })();
