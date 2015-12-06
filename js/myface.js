5845debdfae677b76e200b50e06b031a7477521c
Chris Vernon <christopher.vernon@sky.uk>
2015-12-06 20:58:46 +0000

    Refactored to use selector filtering and now filters by squad

--- js/myface.js
+++ js/myface.js
@@ -1,139 +1,3 @@
-(function(){
-
-    var grid = document.getElementById('site-grid'),
-        background = document.getElementById('site-background'),
-        siteLinks = document.getElementsByClassName('site-nav__link');
-
-    function loadPeople(){
-        var contents = '';
-        background.className = '';
-
-        // create the people elements
-        grid.innerHTML = '';
-        skyFaceData.people.forEach(function(person, i){
-
-            var str = '<div class="grid__item"';
-
-            str += ' data-name="' + person.name + '"';
-            str += ' data-discipline="' + person.discipline + '"';
-            str += ' data-squad="' + person.squad.name + '"';
-            str += ' data-tribe="' + person.squad.tribe.name + '">';
-
-            str += '<a class="tile">';
-            str += '<div class="tile__inner">';
-
-            str += '<div class="tile__image-wrap">';
-            str += '<img class="tile__image" src="' + person.image +'">';
-            //
-            str += '</div>';
-
-            str += '<div class="tile__body">';
-            str += '<h2 class="tile__heading">' + person.name + '</h2>';
-            str += '<h3 class="tile__sub-heading">' + person.discipline.join(', ') + '</h3>';
-            str += '</div>';
-
-            str += '</div>';
-            str += '</a>';
-            str += '</div>';
-
-            contents += str;
-        });
-
-        grid.innerHTML = contents;
-    }
-
-    function loadTribes(){
-        var contents = '';
-        background.className = 'active';
-        background.style.backgroundImage = 'url(' + skyFaceData.tribes[0].background + ')';
-
-        // create the tribes elements
-        grid.innerHTML = '';
-        skyFaceData.tribes.forEach(function(tribe, i){
-
-            var str = '<div class="grid__item"';
-
-            str += ' data-name="' + tribe.name + '">';
-
-            str += '<a class="tile">';
-            str += '<div class="tile__inner">';
-
-            str += '<div class="tile__image-wrap">';
-            str += '<img class="tile__image" src="' + tribe.image +'">';
-            str += '</div>';
-
-            str += '<div class="tile__body">';
-            str += '<h2 class="tile__heading">' + tribe.name + '</h2>';
-            str += '</div>';
-
-            str += '</div>';
-            str += '</a>';
-            str += '</div>';
-
-            contents += str;
-        });
-
-        grid.innerHTML = contents;
-
-    }
-
-    function loadSquads(){
-        var contents = '';
-        background.className = 'active';
-        background.style.backgroundImage = 'url(' + skyFaceData.squads[0].background + ')';
-
-        // create the tribes elements
-        grid.innerHTML = '';
-        skyFaceData.squads.forEach(function(squad, i){
-
-            var str = '<div class="grid__item"';
-
-            str += ' data-name="' + squad.name + '">';
-
-            str += '<a class="tile">';
-            str += '<div class="tile__inner">';
-
-            str += '<div class="tile__image-wrap">';
-            str += '<img class="tile__image" src="' + squad.image +'">';
-            str += '</div>';
-
-            str += '<div class="tile__body">';
-            str += '<h2 class="tile__heading">' + squad.name + '</h2>';
-            str += '</div>';
-
-            str += '</div>';
-            str += '</a>';
-            str += '</div>';
-
-            contents += str;
-        });
-
-        grid.innerHTML = contents;
-    }
-
-    function loadData(loadTarget){
-        if(loadTarget === "tribes"){
-            loadTribes();
-        }else if(loadTarget === "squads"){
-            loadSquads();
-        }else{
-            loadPeople();
-        }
-    }
-
-    // add the event to handle clicks
-    for(var i = 0; i < siteLinks.length; i++){
-        siteLinks[i].addEventListener("click", function(e){
-            e.preventDefault();
-            var loadTarget = e.target.getAttribute('data-load');
-            loadData(loadTarget);
-        });
-    }
-
-    loadPeople();
-
-})();
-
 
 (function(){
 
@@ -245,7 +109,6 @@
     searchPeople()
   }
 
-
 })();
 
 function filter(name){
@@ -253,4 +116,183 @@ function filter(name){
     window.style.textContent = (css);
 }
 
+function filterByClass(cls){
+    var css = '.grid__item:not(' + cls + ') {display: none}';
+    window.style.textContent = (css);
+}
 
+function filterBySquad(name){
+    var css = '.grid__item:not(.grid__item--people) {display: none}';
+    css += '\n .grid__item:not([data-squad="' + name + '"]) {display: none}';
+    window.style.textContent = (css);
+}
+
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
+(function(){
+
+    var grid = document.getElementById('site-grid'),
+        wrapper = document.getElementsByClassName('site-wrapper')
+        background = document.getElementById('site-background'),
+        siteLinks = document.getElementsByClassName('site-nav__link'),
+        contents = '',
+        peopleLoaded = false,
+        tribesLoaded = false,
+        squadsLoaded = false;
+
+    function loadPeople(){
+        background.className = '';
+
+        if(!peopleLoaded){
+
+            // create the people elements
+            skyFaceData.people.forEach(function(person, i){
+
+                var str = '<div class="grid__item grid__item--people"';
+
+                str += ' data-name="' + person.name + '"';
+                str += ' data-discipline="' + person.discipline + '"';
+                str += ' data-squad="' + person.squad.name + '"';
+                str += ' data-tribe="' + person.squad.tribe.name + '">';
+
+                str += '<a class="tile">';
+                str += '<div class="tile__inner">';
+
+                str += '<div class="tile__image-wrap">';
+                str += '<img class="tile__image" src="' + person.image +'">';
+                //
+                str += '</div>';
+
+                str += '<div class="tile__body">';
+                str += '<h2 class="tile__heading">' + person.name + '</h2>';
+                str += '<h3 class="tile__sub-heading">' + person.discipline.join(', ') + '</h3>';
+                str += '</div>';
+
+                str += '</div>';
+                str += '</a>';
+                str += '</div>';
+
+                contents += str;
+            });
+
+            grid.innerHTML = contents;
+            peopleLoaded = true;
+        }
+
+        filterByClass('.grid__item--people');
+    }
+
+    function loadTribes(){
+        background.className = '';
+
+        if(!tribesLoaded){
+
+            // create the tribes elements
+            skyFaceData.tribes.forEach(function(tribe, i){
+
+                var str = '<div class="grid__item grid__item--tribe"';
+
+                str += ' data-name="' + tribe.name + '">';
+
+                str += '<a class="tile grid__link--tribe" href="#">';
+                str += '<div class="tile__inner">';
+
+                str += '<div class="tile__image-wrap">';
+                str += '<img class="tile__image" src="' + tribe.image +'">';
+                str += '</div>';
+
+                str += '<div class="tile__body">';
+                str += '<h2 class="tile__heading">' + tribe.name + '</h2>';
+                str += '</div>';
+
+                str += '</div>';
+                str += '</a>';
+                str += '</div>';
+
+                contents += str;
+            });
+
+            grid.innerHTML = contents;
+            tribesLoaded = true;
+        }
+
+        filterByClass('.grid__item--tribe');
+    }
+
+    function loadSquads(){
+        background.className = '';
+
+        if(!squadsLoaded){
+
+            // create the tribes elements
+            skyFaceData.squads.forEach(function(squad, i){
+
+                var str = '<div class="grid__item grid__item--squad"';
+                str += ' data-background="' + squad.background + '"';
+                str += ' data-name="' + squad.name + '">';
+
+                str += '<a class="tile grid__link--squad" href="#">';
+                str += '<div class="tile__inner">';
+
+                str += '<div class="tile__image-wrap">';
+                str += '<img class="tile__image" src="' + squad.image +'">';
+                str += '</div>';
+
+                str += '<div class="tile__body">';
+                str += '<h2 class="tile__heading">' + squad.name + '</h2>';
+                str += '</div>';
+
+                str += '</div>';
+                str += '</a>';
+                str += '</div>';
+
+                contents += str;
+            });
+
+            grid.innerHTML = contents;
+            initSquadLinks();
+            squadsLoaded = true;
+        }
+
+        filterByClass('.grid__item--squad');
+    }
+
+    function initSquadLinks(){
+        var squadLinks = document.getElementsByClassName('grid__link--squad');
+        // add the event to handle clicks
+        for(var i = 0; i < squadLinks.length; i++){
+            squadLinks[i].addEventListener("click", function(e){
+                e.preventDefault();
+                var loadTarget = this.parentNode.getAttribute('data-name'),
+                    bg = this.parentNode.getAttribute('data-background');
+                filterBySquad(loadTarget);
+                background.className = 'active';
+                background.style.backgroundImage = 'url(' + bg + ')';
+            });
+        }
+    }
+
+    function loadData(loadTarget){
+        if(loadTarget === "tribes"){
+            loadTribes();
+        }else if(loadTarget === "squads"){
+            loadSquads();
+        }else{
+            loadPeople();
+        }
+    }
+
+    // add the event to handle clicks
+    for(var i = 0; i < siteLinks.length; i++){
+        siteLinks[i].addEventListener("click", function(e){
+            e.preventDefault();
+            var loadTarget = e.target.getAttribute('data-load');
+            loadData(loadTarget);
+        });
+    }
+
+    loadPeople();
+
+})();
